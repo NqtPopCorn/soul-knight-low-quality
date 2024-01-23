4040ccf8997efd1fc501821adbf26a58c12cad40
truong <truong263111@gmail.com>
2024-01-23 15:08:51 +0700

    dat bien screen va snake size

--- snakegame.py
+++ snakegame.py
@@ -3,8 +3,9 @@ import time
 import random
 
 pygame.init()
-screen = (500,400)
-display=pygame.display.set_mode(screen)
+SCREEN = (600,400)
+SNAKE_SIZE = 20
+display=pygame.display.set_mode(SCREEN)
 pygame.display.set_caption('snake game')
 
 blue=(0,0,255)
@@ -15,23 +16,23 @@ clock=pygame.time.Clock()
 font_style=pygame.font.SysFont(None,20)
 
 
-def draw_snake(snake_block,snake_list):
+def draw_snake(snake_list):
     for x in snake_list:
-        pygame.draw.rect(display,blue,[x[0],x[1],snake_block, snake_block])
+        pygame.draw.rect(display,blue,[x[0],x[1],SNAKE_SIZE, SNAKE_SIZE])
 def message(msg,color):
     mesg=font_style.render(msg, True, color)
     display.blit(mesg, [10,300])
 def gameLoop():
     game_over=False
     game_close=False
-    x1=400
-    y1=300
+    x1=int(SCREEN[0]/2)
+    y1=int(SCREEN[1]/2)
     x1_change=0 
     y1_change=0
     snake_List=[(x1,y1)]
     Length_of_snake=1
-    foodx=round(random.randrange(0,int(screen[0]/10.0)))*10.0
-    foody=round(random.randrange(0,int(screen[1]/10.0)))*10.0
+    foodx=round(random.randrange(0,int((SCREEN[0]-SNAKE_SIZE)/(SNAKE_SIZE*1.0))))*SNAKE_SIZE*1.0
+    foody=round(random.randrange(0,int((SCREEN[1]-SNAKE_SIZE)/(SNAKE_SIZE*1.0))))*SNAKE_SIZE*1.0
     while not game_over:
         while game_close==True:
             display.fill(white)
@@ -51,28 +52,28 @@ def gameLoop():
         #draw
         display.fill(white)
         #draw food
-        pygame.draw.rect(display,red,[foodx,foody,10,10])
+        pygame.draw.rect(display,red,[foodx,foody,SNAKE_SIZE,SNAKE_SIZE])
         #draw snake
-        draw_snake(10,snake_List)
+        draw_snake(snake_List)
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    if x1_change != 10: x1_change = -10
+                    if x1_change != SNAKE_SIZE: x1_change = -SNAKE_SIZE
                     y1_change = 0
                 elif event.key == pygame.K_RIGHT:
-                    if x1_change != -10: x1_change = 10
+                    if x1_change != -SNAKE_SIZE: x1_change = SNAKE_SIZE
                     y1_change = 0
                 elif event.key == pygame.K_UP:
-                    if y1_change != 10: y1_change = -10
+                    if y1_change != SNAKE_SIZE: y1_change = -SNAKE_SIZE
                     x1_change = 0
                 elif event.key == pygame.K_DOWN:
-                    if y1_change != -10: y1_change = 10
+                    if y1_change != -SNAKE_SIZE: y1_change = SNAKE_SIZE
                     x1_change = 0
         
-        if x1+x1_change>screen[0] or x1+x1_change<0 or y1+y1_change>screen[1] or y1+y1_change<0:
+        if x1+x1_change>SCREEN[0] or x1+x1_change<0 or y1+y1_change>SCREEN[1] or y1+y1_change<0:
             game_close=True
 
         
@@ -89,11 +90,11 @@ def gameLoop():
             del snake_List[0]
 
         if x1 == foodx and y1 == foody:
-            foodx=round(random.randrange(0,int(screen[0]/10.0)))*10.0
-            foody=round(random.randrange(0,int(screen[1]/10.0)))*10.0
+            foodx=round(random.randrange(0,int((SCREEN[0]-SNAKE_SIZE)/(SNAKE_SIZE*1.0))))*SNAKE_SIZE*1.0
+            foody=round(random.randrange(0,int((SCREEN[1]-SNAKE_SIZE)/(SNAKE_SIZE*1.0))))*SNAKE_SIZE*1.0
             Length_of_snake+=1
 
-        clock.tick(20) # thoi gian gioi han cho vong
+        clock.tick(10) # thoi gian gioi han cho vong
     pygame.quit()
    
     
